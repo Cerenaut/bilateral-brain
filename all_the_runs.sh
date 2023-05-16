bded2bd511d47a7abf04a7da3e339212bd3c78ff
Gideon Kowadlo <gideon@agi.io>
2023-05-16 17:13:10 +1000

    update configs

--- all_the_runs.sh
+++ all_the_runs.sh
@@ -19,7 +19,7 @@ echo ----------------------------------------------------------------
 
 # train 5 specialized hemispheres, both fine and coarse
 # and then train the bilateral model using them, each time with a different seed
-python train_system.py --arch vgg11 --num_seeds 5 --epochs 180 180 100 --uni_base_config configs/config_unilateral_specialize.yaml --bi_base_config configs/config_bilateral_specialized.yaml
+python train_system.py --arch vgg11 --num_seeds 2 --epochs 180 180 100 --uni_base_config configs/config_unilateral_specialize.yaml --bi_base_config configs/config_bilateral_specialized.yaml
 
 # if you already have the checkpoints (in trained_models.yaml) you can just run this:
 # python train_system.py --arch vgg11 --num_seeds 5 --epochs 1 1 1 --bi_base_config configs/config_bilateral_specialized.yaml --trained_models configs/trained_models.yaml 
