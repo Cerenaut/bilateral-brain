50ebdd872c69815560c67bcd29490e9a8ce08173
Gideon Kowadlo <gideon@agi.io>
2023-12-25 16:15:12 +1100

    renet9 sub components to get references to layers

--- models/resnet.py
+++ models/resnet.py
@@ -178,32 +178,47 @@ class ResNet9(nn.Module):
     def __init__(self):
         super(ResNet9, self).__init__()
 
-        self.conv = nn.Sequential(
+        self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(num_features=64, momentum=0.9),
-            nn.ReLU(inplace=False),
+            nn.ReLU(inplace=False))
+        
+        self.conv2 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3,  padding=1, bias=False),
             nn.BatchNorm2d(num_features=128, momentum=0.9),
             nn.ReLU(inplace=False),
-            nn.MaxPool2d(2),
-            ResidualBlock(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.MaxPool2d(2))
+        
+        self.res1 = ResidualBlock(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+
+        self.conv3 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(num_features=256, momentum=0.9),
             nn.ReLU(inplace=False),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        
+        self.conv4 = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(num_features=512, momentum=0.9),
             nn.ReLU(inplace=False),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        
+        self.res2 = nn.Sequential(
             ResidualBlock(in_channels=512, out_channels=512, kernel_size=3, padding=1),
             nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Flatten(),
-        )
+            nn.Flatten())
 
+        self.net = nn.Sequential(self.conv1, 
+                                 self.conv2, 
+                                 self.res1, 
+                                 self.conv3, 
+                                 self.conv4, 
+                                 self.res2)
+        
         self.num_features = 512
 
     def forward(self, x):
-        out = self.conv(x)
+        out = self.net(x)
         return out
 
 def resnet9(args):
